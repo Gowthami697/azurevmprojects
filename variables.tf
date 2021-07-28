82cbfda4b3c4f86ebe22a3fe3679ebeb4a4e683c
Ponnusamy <surendhar.ponnusamy@accenture.com>
2021-07-28 19:30:59 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "subscription_id" {
+	default="50fa14bf-5cd8-439f-bb01-956e20e1a4cd"
+}
+
+variable "tenant_id" {
+	default="fc28cb31-dd37-489f-9ec3-a3f4b5cf434b"
+}
+
+variable "client_secret" {
+	default="IXcxI2O_fzCmye.ds_ad6LM7kAP0_VKsX5"
+}
+
+variable "client_id" {
+	default="25eebeb6-1dce-43be-b879-60a30c27a426"
+}
+
+variable "rgname" {
+	default="surendharrg"
+}
+
+variable "location" {
+	default="West US"
+}
